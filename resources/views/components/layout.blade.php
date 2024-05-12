048c24d4c4ba890539202fedd8e43133998a0f86
Junior <duartejusanca@gmail.com>
2024-05-12 20:01:55 +0200

    1)create a components folder, 2)create a layout file inside, 3)install vite, 4)install tailwind, 5)create a folder image on resources

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pixel positions</title>
+    @vite(['resources/css/app.js','resources/js/app.js'])
+</head>
+<body>
+    <div>
+        <nav>
+            <div>
+                <a href="/">
+                    <img src="{{ Vite::asset("resources/images/logo.png")}}" alt="logo" width="20px">
+                </a>
+            </div>
+
+            <div>
+                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaires</a>
+                <a href="#">Companies</a>
+            </div>
+
+            <div>
+                <a href="#">Post a job</a>
+            </div>
+        </nav>
+
+        <main>
+            {{{ $slot }}}
+        </main>
+
+    </div>
+
+</body>
+</html>
