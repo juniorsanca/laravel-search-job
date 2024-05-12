0342174665b13f88fa88ac82470f1219bd2e803b
Junior <duartejusanca@gmail.com>
2024-05-12 23:33:07 +0200

    start frontend developement, create components jobs, section heading, tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel positions</title>
-    @vite(['resources/css/app.js','resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body>
-    <div>
-        <nav>
+<body class="bg-black text-white">
+    <div class="px-10">
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
                 <a href="/">
                     <img src="{{ Vite::asset("resources/images/logo.png")}}" alt="logo" width="20px">
                 </a>
             </div>
 
-            <div>
+            <div class="space-x-6 font-bold">
                 <a href="#">Jobs</a>
                 <a href="#">Careers</a>
                 <a href="#">Salaires</a>
@@ -28,7 +28,7 @@
             </div>
         </nav>
 
-        <main>
+        <main class="mt-10 max-w-[986px] mx-auto">
             {{{ $slot }}}
         </main>
 
