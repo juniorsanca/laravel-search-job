b35f7858b9ff30a6d92f4a9a22e13dfb9da9c43c
Junior <duartejusanca@gmail.com>
2024-05-13 22:24:34 +0200

    add google font on tailwind config file, create new component card

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+    <div>
+        <img src="http://picsum.photos/seed/{{ rand(0,1000) }}/100/100" alt="" class="rounded-xl">
+    </div>
+
+    <div class="flex-1">
+        <a href="#" class="self-start text-sm text-gray-400">Laracast</a>
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
