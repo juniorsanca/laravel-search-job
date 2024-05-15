b79a41274f9d647e008efa9d8fa9b353753f39c9
Junior <duartejusanca@gmail.com>
2024-05-15 23:48:23 +0200

    Create a AAA, Arrange, Act, Assert

--- tests/Unit/JobTest.php
+++ tests/Unit/JobTest.php
@@ -1,5 +1,26 @@
 <?php
 
+// test and it -> do the same thing !
+// php artisan make:test
+// php artisan test
+
+use App\Models\Employer;
+use App\Models\Job;
+
 test('example', function () {
     expect(true)->toBeTrue();
 });
+it('', function () {});
+
+test('', function() {
+// AAA -> Arrange, Act, Assert
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    // Act and Assert
+    expect($job->employer->is($employer))->toBeTrue();
+});
+
+
