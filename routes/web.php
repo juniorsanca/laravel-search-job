f949b0e4bb16d0b9272a408896b2d607b00610ca
Junior <duartejusanca@gmail.com>
2024-05-18 21:27:18 +0200

    create new routes for user authentication

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/',[\App\Http\Controllers\JobController::class, 'index']);
+Route::get('/', [JobController::class, 'index']);
+
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [\App\Http\Controllers\RegisterUserController::class, 'store']);
+
+Route::get('/login', [\App\Http\Controllers\SessionController::class, 'create']);
+Route::post('/login', [\App\Http\Controllers\SessionController::class, 'store']);
+Route::delete('/logout', [\App\Http\Controllers\SessionController::class, 'destroy']);
