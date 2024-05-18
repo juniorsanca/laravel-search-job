2f977db8cc25135a4f7d34ff18a623cab25d350f
Junior <duartejusanca@gmail.com>
2024-05-18 21:21:16 +0200

    create new folder FORM on components

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,3 @@
-{{-- <a href="#" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300 ">{{ $slot }}</a> --}}
 @props(['tag','size' => 'base'])
 
 @php
@@ -12,7 +11,5 @@
         $classes .= " px-3 py-1 text-2xs";
     }
 @endphp
-
-
 
 <a href="/tags/{{ strtolower($tag->name)}}" class="{{ $classes }}">{{ $tag->name }}</a>
