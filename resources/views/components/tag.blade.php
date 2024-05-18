e03ac919edbe079a68258369b0f8c8d24e8600d0
Junior <duartejusanca@gmail.com>
2024-05-18 19:28:23 +0200

    correct errors space on the tag file

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -5,11 +5,11 @@
     $classes = "bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300";
 
     if ($size === 'base') {
-        $classes = "bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300 px-5 py-1 text-sm";
+        $classes .= " px-5 py-1 text-sm";
     }
 
     if ($size === 'small') {
-        $classes = "bg-white/10 hover:bg-white/25 rounded-xl font-bold transition-colors duration-300 px-3 py-1 text-2xs";
+        $classes .= " px-3 py-1 text-2xs";
     }
 @endphp
 
