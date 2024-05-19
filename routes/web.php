79ebaac0b46a42684c80851ea278a307f05e2b34
Junior <duartejusanca@gmail.com>
2024-05-19 23:25:35 +0200

    create register files on auth folder and create a component heading

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -10,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [JobController::class, 'index']);
 
 Route::get('/register', [RegisterUserController::class, 'create']);
-Route::post('/register', [\App\Http\Controllers\RegisterUserController::class, 'store']);
+Route::post('/register', [RegisterUserController::class, 'store']);
 
-Route::get('/login', [\App\Http\Controllers\SessionController::class, 'create']);
-Route::post('/login', [\App\Http\Controllers\SessionController::class, 'store']);
-Route::delete('/logout', [\App\Http\Controllers\SessionController::class, 'destroy']);
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/logout', [SessionController::class, 'destroy']);
